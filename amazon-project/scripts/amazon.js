d7fb0e72252d502aaab41c1797f2499310ce7db3
nataliPerezG <nataperezg22@gmail.com>
2025-02-08 16:32:03 -0500

    Use modules an import cart from cart.js and products from products.js
    Signed-off-by: nataliPerezG <nataperezg22@gmail.com>

--- amazon-project/scripts/amazon.js
+++ amazon-project/scripts/amazon.js
@@ -1,4 +1,5 @@
-
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
 // Datos desde el archivo data/products
 
 let productHTML = "";
@@ -94,4 +95,5 @@ buttonAddCart.forEach((button) => {
 
   })
 })
+
 
