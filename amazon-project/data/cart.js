c06859e4876dada3a0bc1538aeecde8bc466cc09
nataliPerezG <nataperezg22@gmail.com>
2025-02-09 12:54:57 -0500

    Al intentar actualizar los datos del carrito en el localStorage me encontré con un problema que no puedo resolver en este punto. Volveré a él después
    Signed-off-by: nataliPerezG <nataperezg22@gmail.com>

--- amazon-project/data/cart.js
+++ amazon-project/data/cart.js
@@ -1,13 +1,26 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 25
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 10
-  }
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+console.log(cart);
+
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1
+    }
+  ]
+}
+
+console.log(cart)
+
+
+const saveToStorage = () => {
+  localStorage.setItem("cart", JSON.stringify(cart))
+}
 
 export const addProductCart = (productId) => {
   let existProduct;
@@ -21,21 +34,20 @@ export const addProductCart = (productId) => {
   } else {
     cart.push({
       productId,
-      quantity: 1
+      quantity: 2
     })
   }
+  saveToStorage()
 }
 
 export const removeFromCart = (productId) => {
   let newCart = [];
-
-  cart.forEach((itemProduct) => {
-    if (itemProduct.productId !== productId) {
-      newCart.push(itemProduct)
+  cart.forEach((item) => {
+    if (item.productId !== productId) {
+      newCart.push(item)
     }
   })
-
   cart = newCart;
-
+  saveToStorage()
 }
 
--- amazon-project/scripts/checkout.js
+++ amazon-project/scripts/checkout.js
@@ -3,28 +3,35 @@ import { products } from "../data/products.js";
 import { formatMoney } from "../utils/money.js";
 
 
-let orderSumary = document.querySelector(".order-summary")
+let orderSumary =
+  document.querySelector(".order-summary");
+
 let productHtml = "";
 
-cart.forEach((cartItem) => {
-  let productId = cartItem.productId;
-  let sameProduct;
+
+console.log(cart);
+
+let sameProduct;
+
+cart.forEach((item) => {
+  let productId = item.productId;
 
   products.forEach((product) => {
+
     if (product.id === productId) {
       sameProduct = product;
     }
   })
 
   productHtml += `
-  <div class="cart-item-container"
-  id="product-${productId}">
-
+  <div class="cart-item-container
+  js-item-${sameProduct.id}">
   <div class="delivery-date">
     Delivery date: Tuesday, June 21
   </div>
   <div class="cart-item-details-grid">
-    <img class="product-image" src=${sameProduct.image}>
+    <img class="product-image"
+    src=${sameProduct.image}>
     <div class="cart-item-details">
       <div class="product-name">
         ${sameProduct.name}
@@ -34,25 +41,28 @@ cart.forEach((cartItem) => {
       </div>
       <div class="product-quantity">
         <span>
-          Quantity: <span class="quantity-label">
-          ${cartItem.quantity}</span>
+          Quantity: <span 
+          class="quantity-label">
+          ${item.quantity}</span>
         </span>
-        <span class="update-quantity-link link-primary">
+        <span class="update-quantity-link 
+        link-primary">
           Update
         </span>
-        <span class="delete-quantity-link link-primary"
+        <span class="delete-quantity-link 
+        link-primary"
         data-product-id="${sameProduct.id}">
           Delete
         </span>
       </div>
     </div>
-
     <div class="delivery-options">
       <div class="delivery-options-title">
         Choose a delivery option:
       </div>
       <div class="delivery-option">
-        <input type="radio" checked class="delivery-option-input"
+        <input type="radio" checked 
+        class="delivery-option-input"
         name="delivery-option-${sameProduct.id}">
         <div>
           <div class="delivery-option-date">
@@ -64,7 +74,8 @@ cart.forEach((cartItem) => {
         </div>
       </div>
       <div class="delivery-option">
-        <input type="radio" class="delivery-option-input"
+        <input type="radio" 
+        class="delivery-option-input"
         name="delivery-option-${sameProduct.id}">
         <div>
           <div class="delivery-option-date">
@@ -76,7 +87,8 @@ cart.forEach((cartItem) => {
         </div>
       </div>
       <div class="delivery-option">
-        <input type="radio" class="delivery-option-input"
+        <input type="radio" 
+        class="delivery-option-input"
         name="delivery-option-${sameProduct.id}">
         <div>
           <div class="delivery-option-date">
@@ -92,18 +104,20 @@ cart.forEach((cartItem) => {
 </div>`
 })
 
+
 orderSumary.innerHTML = productHtml;
 
-let deleteLinks = document.querySelectorAll(".delete-quantity-link");
+let deleteLinks =
+  document.querySelectorAll(".delete-quantity-link");
 
 deleteLinks.forEach((link) => {
 
   link.addEventListener("click", () => {
-    let productId = link.dataset.productId
-    removeFromCart(productId)
-    let container = document.getElementById(`product-${productId}`)
-    container.remove();
+    let productId = link.dataset.productId;
+    console.log(productId)
+    removeFromCart(productId);
+    let container =
+      document.querySelector(`.js-item-${productId}`)
+    container.remove()
   })
-
 })
-
