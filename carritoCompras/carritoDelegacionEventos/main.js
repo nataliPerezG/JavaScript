15fd7f609cd24da2a80680b0905ee448ea93fe79
nataliPerezG <nataperezg22@gmail.com>
2025-02-18 11:06:27 -0500

    Carrito de compras con delegación de eventos
    Signed-off-by: nataliPerezG <nataperezg22@gmail.com>

--- carritoCompras/carritoDelegacionEventos/main.js
+++ carritoCompras/carritoDelegacionEventos/main.js
@@ -1,49 +1,117 @@
+// Elementos desde html:
+let plantCarrito =
+  document.getElementById("template-carrito");
+let frag =
+  document.createDocumentFragment();
 let carrito = document.getElementById("carrito");
-let plantillaCarrito = document.getElementById("template-carrito");
-let fragmento = document.createDocumentFragment();
-let total = document.getElementById("total");
-let planitlllaFooter = document.getElementById("template-footer");
+let plantFooter =
+  document.getElementById("template-footer");
+let footer = document.getElementById("total");
 
 
+//Array para añadir los productos al carrito
+let arrayProductos = [];
+
+
+//Delegación de eventos:
 document.addEventListener("click", (e) => {
-  if (e.target.matches(".btn-agregar")) {
-    agregarAlCarrito(e)
-    pintarCarrito(e)
-
+  if (e.target.className === "btn-agregar") {
+    agregarProducto(e)
+  }
+  if (e.target.className === "agregar") {
+    aumentar(e)
+  }
+  if (e.target.className === "eliminar") {
+    eliminar(e)
   }
 })
 
-let arrayCarrito = []
-
-
-let agregarAlCarrito = (e) => {
-
-  console.log(e.target.dataset.nombre);
-
+// Funciones:
+const agregarProducto = (e) => {
   let producto = {
     nombre: e.target.dataset.nombre,
-    id: e.target.dataset.nombre,
-    cantidad: 1,
-    precio: parseInt(e.target.dataset.precio),
+    precio: +(e.target.dataset.precio),
+    cant: 1,
   }
 
-  //Recorre el array para saber si el producto está o no agregado
-  let indice = arrayCarrito.findIndex(item => item.id === producto.id)
-  console.log(indice)
+  //verificar si el producto ya está en el array:
+  let indice =
+    arrayProductos.findIndex(item =>
+      item.nombre === producto.nombre
+    );
 
   if (indice === -1) {
-    arrayCarrito.push(producto)
+    arrayProductos.push(producto)
   } else {
-    arrayCarrito[indice].cantidad++
-    arrayCarrito[indice].precio =
-      arrayCarrito[indice].cantidad * producto.precio;
+    arrayProductos[indice].cant++
   }
-
+  pintarProductos()
 }
 
-let pintarCarrito = (e) => {
+const pintarProductos = () => {
 
-  let clone = plantillaCarrito.content.cloneNode(true)
+  /*   Limpiar el contenido para que 
+    no se repita en cada iteración: */
+  carrito.textContent = "";
 
+  arrayProductos.forEach((item) => {
+    let clone = plantCarrito.content.cloneNode(true);
+    clone.querySelector(".cant").textContent = item.cant;
+    clone.querySelector(".nombre").textContent =
+      item.nombre;
+    clone.querySelector("span").textContent =
+      item.precio * item.cant;
 
+    //Obtener botones dinámicos;
+    let agregar = clone.querySelector(".agregar");
+    let eliminar = clone.querySelector(".eliminar");
+
+    // Añadir un dataset para vincularlo con el producto
+    agregar.dataset.id = item.nombre;
+    eliminar.dataset.id = item.nombre;
+
+    frag.appendChild(clone)
+  })
+  carrito.appendChild(frag);
+  pintarFooter()
+}
+
+const pintarFooter = () => {
+
+  footer.textContent = "";
+
+  //Sumar los totales de cada producto:
+  let totalCompra = arrayProductos.reduce(
+    (acu, current) => acu + current.cant * current.precio, 0
+  )
+  let clone = plantFooter.content.cloneNode(true);
+  clone.querySelector("span").textContent = totalCompra;
+
+  if (totalCompra === 0) {
+    clone.textContent = "";
+  }
+
+  footer.appendChild(clone)
+}
+
+const aumentar = (e) => {
+  arrayProductos =
+    arrayProductos.map((item) => {
+      if (item.nombre === e.target.dataset.id) {
+        item.cant++
+      } return item
+    })
+  pintarProductos()
+}
+
+const eliminar = (e) => {
+  arrayProductos = arrayProductos.filter(item => {
+    if (item.nombre === e.target.dataset.id) {
+      if (item.cant > 0) {
+        item.cant--;
+        if (item.cant === 0) return
+      }
+    } return item
+  })
+  pintarProductos()
 }
