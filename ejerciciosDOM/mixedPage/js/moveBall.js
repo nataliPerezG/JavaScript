5a304f9cc1f68e8a2047f0e85b9e3cd3f96b80f6
nataliPerezG <nataperezg22@gmail.com>
2025-02-23 18:53:24 -0500

    Sección tres: movimiento con el teclado
    Signed-off-by: nataliPerezG <nataperezg22@gmail.com>

---
+++ ejerciciosDOM/mixedPage/js/moveBall.js
@@ -0,0 +1,45 @@
+
+let y = 0;
+let x = 0;
+
+export function moveBall(e, stage, ball) {
+
+  let escenario = document.querySelector(stage);
+  let bola = document.querySelector(ball);
+  let limitBall = bola.getBoundingClientRect();
+  let limitStage = escenario.getBoundingClientRect();
+
+  switch (e.keyCode) {
+    case 37:
+      if (limitBall.left > limitStage.left) {
+        x--;
+        e.preventDefault();
+      }
+      break;
+    case 38:
+      if (limitBall.top > limitStage.top) {
+        y--;
+        // Para que no se mueva la barra de scroll
+        e.preventDefault();
+      }
+      break;
+    case 39:
+      if (limitBall.right < limitStage.right) {
+        x++;
+        e.preventDefault();
+      }
+      break;
+    case 40:
+      if (limitBall.bottom < limitStage.bottom) {
+        y++;
+        e.preventDefault();
+      }
+      break;
+    default:
+      console.log("Tecla no asignada")
+      break;
+  }
+
+  bola.style.transform = `translate(${x * 5}px,${y * 5}px)`
+
+}
--- ejerciciosDOM/mixedPage/mixedPage.js
+++ ejerciciosDOM/mixedPage/mixedPage.js
@@ -1,11 +1,16 @@
 
 import { showMenu } from "./js/buergerMenu.js";
 import { reloj, alarma } from "./js/relojAlarma.js";
+import { moveBall } from "./js/moveBall.js";
 
 document.addEventListener("DOMContentLoaded", e => {
   showMenu(".hamburger", ".panel", "is-active", ".menu a");
   reloj("h4", ".act-reloj", ".desact-reloj");
   alarma(".alarma", ".act-alarma", ".desact-alarma");
+})
+
+document.addEventListener("keydown", e => {
+  moveBall(e, ".stage", ".ball")
 })
 
 
